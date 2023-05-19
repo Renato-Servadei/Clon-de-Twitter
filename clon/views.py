6b3827c6368f0bff46d4c0e926f96fefde5bd24d
Rober <boblawyer1@gmail.com>
2023-05-19 20:26:28 -0300

    implementado el registro

--- clon/views.py
+++ clon/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Profile
+from .forms import UserRegisterForm
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'twitter/newsfeed.html', context={'posts': posts})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'twitter/register.html', context={'form': form})
