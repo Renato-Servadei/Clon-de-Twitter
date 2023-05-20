54d46849a1b8bbc9c8a6fb5400599bcc50365790
Rober <boblawyer1@gmail.com>
2023-05-20 17:42:27 -0300

    implementado el uso de imagenes

--- clon/models.py
+++ clon/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=100, default="Hola, Rufus")
-    #image
+    image = models.ImageField(default='default.png')
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
--- clon/urls.py
+++ clon/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
@@ -8,4 +10,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="twitter/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
